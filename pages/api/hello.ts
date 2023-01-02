aa9cc9e6ece2283c7c4989d068d1c3aac0cb1ca4
Gelar Rustiawan <gelargew@gmail.com>
2023-01-02 17:01:41 +0700

    api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
 export default function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<any>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  res.status(200).json({
+    name: "John Doe",
+    items: [
+      { id: 1, name: "item 1" },
+      { id: 2, name: "item 2" },
+      { id: 3, name: "item 3" },
+    ],
+  });
 }
